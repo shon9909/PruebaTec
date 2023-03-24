1ef91ba7ce2d79abd4058a1fb7d943a4294a45f6
Nicolas Infante <nicolasgiovanny@hotmail.com>
2023-03-23 20:27:34 -0500

    api conection modificada

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,13 @@
 class ApiController < ApplicationController
-    require 'connection'
-    def index
-        @post = Connection.get('/products')
-    end
+  require 'faraday'
+  def index
+    @conn = Faraday.new(url: 'https://dummyjson.com')
+    get_products = @conn.get('/products')
+    @products = JSON.parse(get_products.body)
+    if get_products.success?
+      render 'index'
+    else
+      raise StandardError, "error al conectarse a la api"
+     end
+  end
 end
